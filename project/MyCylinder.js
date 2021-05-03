0ccaed7333e671debb7bd55f2f7075e0ff54269b
Sofia Teixeira <up201806629@fe.up.pt>
2021-05-03 20:06:05 +0100

    PartA - 2.2 - adding top and bottom faces

--- project/MyCylinder.js
+++ project/MyCylinder.js
@@ -16,6 +16,44 @@ export class MyCylinder extends CGFobject {
         this.indices = [];
         this.normals = [];
 
+        var ang = 0;
+        var alphaAng = 2*Math.PI/this.slices;
+        
+        // top face
+        this.vertices.push(0, 1, 0);
+        for(var i = 1; i <= this.slices; i++){
+            this.vertices.push(Math.cos(ang), 1, -Math.sin(ang));
+            if (i == this.slices - 1)
+                this.indices.push(i, this.slices, 0);
+            else
+                this.indices.push(i, (i + 1) % this.slices, 0);
+            this.normals.push(Math.cos(ang), Math.cos(Math.PI/4.0), -Math.sin(ang));
+            ang+=alphaAng;
+        }
+
+        // bottom face
+        ang = 0;
+        this.vertices.push(0, 0, 0);
+        for(var i = this.slices+2; i <= this.slices * 2 + 1; i++){
+            this.vertices.push(Math.cos(ang), 0, -Math.sin(ang));
+            if (i == this.slices * 2)
+                this.indices.push(this.slices+1, this.slices * 2 + 1, i);
+            else if (i == this.slices * 2 + 1)
+                this.indices.push(this.slices+1, this.slices + 2, i);
+            else
+                this.indices.push(this.slices+1, (i+1)%(this.slices*2+1), i);
+            this.normals.push(Math.cos(ang), -Math.cos(Math.PI/4.0), -Math.sin(ang));
+            ang+=alphaAng;
+        }
+/*
+        // side faces
+        for (var i = 0; i < this.slices; i++){
+            this.indices.push(this.slices, (i+1) % (this.slices), i);
+            //this.indices.push(this.slices, (i+1) % (this.slices), i);
+            ang+=alphaAng;
+        }
+*/
+
         this.primitiveType = this.scene.gl.TRIANGLES;
         this.initGLBuffers();
     }
