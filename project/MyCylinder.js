d1f7ec5f65e21dfe6143f290e0e34cbb8af34dd6
Sofia Teixeira <up201806629@fe.up.pt>
2021-05-04 15:52:06 +0100

    PartA - 2.2 - new cylinder

--- project/MyCylinder.js
+++ project/MyCylinder.js
@@ -1,4 +1,4 @@
-import {CGFobject} from '../lib/CGF.js';
+import {CGFappearance, CGFobject} from '../lib/CGF.js';
 /**
 * MyCylinder
 * @constructor
@@ -9,52 +9,45 @@ export class MyCylinder extends CGFobject {
     constructor(scene, slices) {
         super(scene);
         this.slices = slices;
+        this.initTexture(this.scene);
         this.initBuffers(this.scene);
+    }
+    initTexture() {
+        //------ Texture
+        this.cylinderTexture = new CGFappearance(this.scene);
+        this.cylinderTexture.setAmbient(0.1, 0.1, 0.1, 1);
+        this.cylinderTexture.setDiffuse(0.9, 0.9, 0.9, 1);
+        this.cylinderTexture.setSpecular(0.1, 0.1, 0.1, 1);
+        this.cylinderTexture.setShininess(10.0);
+        this.cylinderTexture.loadTexture('images/earth.jpg');
+        this.cylinderTexture.setTextureWrap('REPEAT', 'REPEAT');
     }
     initBuffers() {
         this.vertices = [];
         this.indices = [];
         this.normals = [];
-
+        this.textureCoords = [];
         var ang = 0;
         var alphaAng = 2*Math.PI/this.slices;
-        
-        // top face
-        this.vertices.push(0, 1, 0);
-        for(var i = 1; i <= this.slices; i++){
-            this.vertices.push(Math.cos(ang), 1, -Math.sin(ang));
-            if (i == this.slices - 1)
-                this.indices.push(i, this.slices, 0);
-            else
-                this.indices.push(i, (i + 1) % this.slices, 0);
-            this.normals.push(Math.cos(ang), Math.cos(Math.PI/4.0), -Math.sin(ang));
-            ang+=alphaAng;
-        }
 
-        // bottom face
-        ang = 0;
-        this.vertices.push(0, 0, 0);
-        for(var i = this.slices+2; i <= this.slices * 2 + 1; i++){
+        this.vertices.push(Math.cos(ang), 0, -Math.sin(ang));
+        this.vertices.push(Math.cos(ang), 1, -Math.sin(ang));
+        ang += alphaAng;    
+        for (var i = 0; i < this.slices * 2; i++){
             this.vertices.push(Math.cos(ang), 0, -Math.sin(ang));
-            if (i == this.slices * 2)
-                this.indices.push(this.slices+1, this.slices * 2 + 1, i);
-            else if (i == this.slices * 2 + 1)
-                this.indices.push(this.slices+1, this.slices + 2, i);
-            else
-                this.indices.push(this.slices+1, (i+1)%(this.slices*2+1), i);
-            this.normals.push(Math.cos(ang), -Math.cos(Math.PI/4.0), -Math.sin(ang));
-            ang+=alphaAng;
-        }
+            this.vertices.push(Math.cos(ang), 1, -Math.sin(ang));
+            
+            this.indices.push(i, i+1, i+2);
+            this.indices.push(i, i+2, i+1);
 
-        // side faces
-        for (var i = 0; i < this.slices; i++){
-            if (i == this.slices - 1) {
-                this.indices.push(this.slices*2+1, this.slices+2, 1);
-                this.indices.push(1, this.slices, this.slices*2+1);
-            }else {
-                this.indices.push(i+2, i+1, (this.slices + 2) + i);
-                this.indices.push((this.slices + 2) + i, (this.slices + 2) + i + 1, i+2);
-            }
+            this.normals.push(Math.cos(ang), 0, -Math.sin(ang));
+            this.normals.push(Math.cos(ang), 0, -Math.sin(ang));
+            
+
+            this.textureCoords.push(0, (1/this.slices)*i);
+            this.textureCoords.push(1, 0);
+            this.cylinderTexture.apply();
+
             ang+=alphaAng;
         }
 
